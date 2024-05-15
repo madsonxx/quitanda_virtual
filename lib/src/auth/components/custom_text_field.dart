0647a55dcfa28d15c5471baf173bc6e73c7d34a1
Madson Caminha <madson8@gmail.com>
2024-05-15 18:16:42 -0400

    Tela de cadastro

--- lib/src/auth/components/custom_text_field.dart
+++ lib/src/auth/components/custom_text_field.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextField extends StatefulWidget {
   final IconData icon;
   final String label;
-
   final bool isSecret;
+  final List<TextInputFormatter>? inputFormatters;
 
   const CustomTextField(
       {super.key,
       required this.icon,
       required this.label,
-      this.isSecret = false});
+      this.isSecret = false,
+      this.inputFormatters});
 
   @override
   State<CustomTextField> createState() => _CustomTextFieldState();
@@ -21,7 +23,6 @@ class _CustomTextFieldState extends State<CustomTextField> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
     isObscure = widget.isSecret;
@@ -32,6 +33,7 @@ class _CustomTextFieldState extends State<CustomTextField> {
     return Padding(
       padding: const EdgeInsets.only(bottom: 15.0),
       child: TextFormField(
+        inputFormatters: widget.inputFormatters,
         obscureText: isObscure,
         decoration: InputDecoration(
           prefixIcon: Icon(widget.icon),
--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:quitanda_virtual/src/auth/components/custom_text_field.dart';
+import 'package:quitanda_virtual/src/auth/sign_up_screen.dart';
 import 'package:quitanda_virtual/src/config/custom_colors.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -8,155 +9,169 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+
     return Scaffold(
       backgroundColor: CustomColors.customSwatchColor,
-      body: Column(
-        children: [
-          Expanded(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          child: Column(
             children: [
-              //Nome do App
-              Text.rich(
-                TextSpan(
-                  style: const TextStyle(fontSize: 40),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const TextSpan(
-                      text: 'Green',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                    //Nome do App
+                    Text.rich(
+                      TextSpan(
+                        style: const TextStyle(fontSize: 40),
+                        children: [
+                          const TextSpan(
+                            text: 'Green',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                              text: 'grocer',
+                              style: TextStyle(
+                                  color: CustomColors.customContrastColor))
+                        ],
                       ),
                     ),
-                    TextSpan(
-                        text: 'grocer',
-                        style:
-                            TextStyle(color: CustomColors.customContrastColor))
+
+                    //Categorias
+                    SizedBox(
+                      height: 30,
+                      child: DefaultTextStyle(
+                        style: const TextStyle(fontSize: 25),
+                        child: AnimatedTextKit(
+                          pause: Duration.zero,
+                          repeatForever: true,
+                          animatedTexts: [
+                            FadeAnimatedText(
+                              'Frutas',
+                            ),
+                            FadeAnimatedText('Verduras'),
+                            FadeAnimatedText('Legumes'),
+                            FadeAnimatedText('Carnes'),
+                            FadeAnimatedText('Cereais'),
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
 
-              //Categorias
-              SizedBox(
-                height: 30,
-                child: DefaultTextStyle(
-                  style: const TextStyle(fontSize: 25),
-                  child: AnimatedTextKit(
-                    pause: Duration.zero,
-                    repeatForever: true,
-                    animatedTexts: [
-                      FadeAnimatedText(
-                        'Frutas',
-                      ),
-                      FadeAnimatedText('Verduras'),
-                      FadeAnimatedText('Legumes'),
-                      FadeAnimatedText('Carnes'),
-                      FadeAnimatedText('Cereais'),
-                    ],
+              //Formulário
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 40,
+                  horizontal: 32,
+                ),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadiusDirectional.vertical(
+                    top: Radius.circular(45),
                   ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    //Email
+                    const CustomTextField(
+                      label: 'email',
+                      icon: Icons.email,
+                    ),
+                    //Senha
+                    const CustomTextField(
+                      label: 'senha',
+                      icon: Icons.lock,
+                      isSecret: true,
+                    ),
+                    //Botão Entrar
+                    SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18))),
+                        onPressed: () {},
+                        child: const Text(
+                          'Entrar',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    //Esqueceu a senha
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Esqueceu a senha',
+                            style: TextStyle(
+                                color: CustomColors.customContrastColor),
+                          )),
+                    ),
+                    //Divisor Ou
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey.withAlpha(90),
+                              thickness: 2.0,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            child: Text('Ou'),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey.withAlpha(90),
+                              thickness: 2.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    //Botão de Novo usuário
+                    SizedBox(
+                      height: 50,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18)),
+                          side: const BorderSide(
+                            width: 2,
+                            color: Colors.green,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (c) {
+                            return SignUpScreen();
+                          }));
+                        },
+                        child: const Text(
+                          'Criar conta',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               )
             ],
-          )),
-
-          //Formulário
-          Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 40,
-              horizontal: 32,
-            ),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadiusDirectional.vertical(
-                top: Radius.circular(45),
-              ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                //Email
-                const CustomTextField(
-                  label: 'email',
-                  icon: Icons.email,
-                ),
-                //Senha
-                const CustomTextField(
-                  label: 'senha',
-                  icon: Icons.lock,
-                  isSecret: true,
-                ),
-                //Botão Entrar
-                SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18))),
-                    onPressed: () {},
-                    child: const Text(
-                      'Entrar',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ),
-                //Esqueceu a senha
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Esqueceu a senha',
-                        style:
-                            TextStyle(color: CustomColors.customContrastColor),
-                      )),
-                ),
-                //Divisor Ou
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 10.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          color: Colors.grey.withAlpha(90),
-                          thickness: 2.0,
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15),
-                        child: Text('Ou'),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          color: Colors.grey.withAlpha(90),
-                          thickness: 2.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                //Botão de Novo usuário
-                SizedBox(
-                  height: 50,
-                  child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18)),
-                      side: const BorderSide(
-                        width: 2,
-                        color: Colors.green,
-                      ),
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Criar conta',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
